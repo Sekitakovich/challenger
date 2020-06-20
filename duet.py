7fb8633b3bcdbe53470c2871f78699adeabcf377
K.Seki <k-seki@klabo.co.jp>
2020-06-20 11:10:56 +0900

    joinの意味を理解しましょう

--- duet.py
+++ duet.py
@@ -1,23 +1,52 @@
 from typing import List
 from itertools import permutations
 import numpy as np
+from loguru import logger
 
-core: List[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-this = permutations(core)
 
-for indexS, src in enumerate(this):
-    L = np.array(src, dtype=int).reshape([3, 3])
-    print(L)
-    for indexD, dst in enumerate(this):
-        if indexD != indexS:
+class Duet(object):
+
+    def __init__(self):
+
+        area = permutations([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.core = [np.array(c, dtype='uint8').reshape([3, 3]) for c in area]
+        self.result = np.zeros([3, 9], dtype='uint8')
+        self.collecton: List[List[int]] = []
+        self.total: int = 0
+
+    def trio(self, *, duet: np.ndarray, indexL: int, indexC: int):
+        self.result[0:3, 0:6] = duet
+        for indexR, R in enumerate(self.core):
+            self.result[0:3, 6:9] = R
             ok = True
-            C = np.array(dst, dtype=int).reshape([3, 3])
-            ddd = np.concatenate([L, C], axis=1)
-            for a in range(3):
-                uuu = np.unique(ddd[a])
-                if uuu.size < 6:
+            for eee in self.result:
+                if np.unique(eee).size < 9:
                     ok = False
                     break
             if ok:
-                print(indexS, indexD)
-                print(ddd)
+                self.collecton.append([indexL, indexC, indexR])
+                self.total += 1
+                # print('at %d: %s' % (indexR, self.result))
+                print('+++ %d at %d:%d:%d' % (self.total, indexL, indexC, indexR))
+                print(self.result)
+
+    def create(self):
+        for indexL, L in enumerate(self.core):
+            for indexC, C in enumerate(self.core):
+                if indexC != indexL:
+                    ok = True
+                    ddd = np.concatenate([L, C], axis=1)
+                    for eee in ddd:
+                        if np.unique(eee).size < 6:
+                            ok = False
+                            break
+                    if ok:
+                        logger.debug('at %d:%d' % (indexL, indexC))
+                        self.trio(duet=ddd, indexL=indexL, indexC=indexC)
+                        # print(ddd)
+                        # self.duet.append(ddd)
+
+
+if __name__ == '__main__':
+    duet = Duet()
+    duet.create()
