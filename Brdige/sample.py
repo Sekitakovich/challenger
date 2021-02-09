c37891bc615b7595151c90f5cd64132e72a38707
k-seki <seki@magnetic.co.jp>
2021-02-09 09:52:53 +0900

    append termcolor

--- Brdige/sample.py
+++ Brdige/sample.py
@@ -1,9 +1,23 @@
 import random
+import sys
 from dataclasses import dataclass
 from enum import IntEnum
-from typing import List
+from typing import List, Dict
 
 from loguru import logger
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<level>{time:HH:mm:ss} {file}:{line}:{function} {message}</level>")
+# logger.add(
+#     'logs/mc.log',
+#     rotation='1 day',
+#     retention=180,
+#     level='WARNING',
+#     encoding='cp932')
+
 from termcolor import colored
 
 
@@ -20,6 +34,12 @@ class Suits(IntEnum):
     H = 2
     D = 3
     C = 4
+
+
+@dataclass()
+class Pointer(object):
+    volume: int = 0
+    series: int = 0
 
 
 @dataclass(frozen=True)  # read only
@@ -49,13 +69,16 @@ class CBox(object):
         self.suit = [Suits.S, Suits.H, Suits.D, Suits.C]
         self.seat = ['S', 'W', 'N', 'E']
         self.name = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
-        self.marks = ['♠', '♥', '♦', '♣']
+        # self.marks = ['♠', '♥', '♦', '♣']
+        self.marks = ['S', 'H', 'D', 'C']
         self.hcps = [4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         # self.fcps = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        self.SQUARE = 4
+        self.GOLGO = 13
 
         index = 0
-        for a in range(4):
-            for b in range(13):
+        for a in range(self.SQUARE):
+            for b in range(self.GOLGO):
                 self.card.append(Card(
                     suit=self.suit[a],
                     name=self.name[b],
@@ -72,10 +95,10 @@ class CBox(object):
     def deal(self) -> List[Hand]:
         result = []
         random.shuffle(self.indexs)
-        for p in range(4):
+        for p in range(self.SQUARE):
             card = []
             hcp = 0
-            for m in sorted(self.indexs[(p * 13): (p * 13) + 13]):
+            for m in sorted(self.indexs[(p * self.GOLGO): (p * self.GOLGO) + self.GOLGO]):
                 this = self.card[m]
                 hcp += this.hcp
                 card.append(this)
@@ -86,20 +109,22 @@ class CBox(object):
 
 if __name__ == '__main__':
 
-    def cont(*, card: List[Card]):
-        series = 0
+    def series(*, card: List[Card]) -> int:
+        result = 0
+        cont = 0
         for c in range(len(card) - 1):
             if card[c].suit == card[c + 1].suit:
                 # logger.info(f'checking {c}')
                 if card[c].index == card[c + 1].index - 1:
-                    series += 1
+                    cont += 1
+                    result += 1
                 else:
-                    if series:
-                        logger.debug(f'{card[c].mark}{card[c].name} = {series}')
-                    series = 0
+                    if cont:
+                        logger.debug(f'{card[c].mark}{card[c].name} = {cont}')
+                    cont = 0
             else:
-                series = 0
-
+                cont = 0
+        return result
 
     def main():
         cbox = CBox()
@@ -107,7 +132,6 @@ if __name__ == '__main__':
 
         for seat, m in enumerate(deal):
             # card = ' '.join([colored(f'{c.mark}{c.name}','red') for c in m.card])
-            cont(card=m.card)
             name = []
             for card in m.card:
                 text = f'{card.mark}{card.name}'
@@ -115,8 +139,10 @@ if __name__ == '__main__':
                 attr = f'{card.attr}'
                 view = colored(text, color, attrs=[f'{attr}'])
                 name.append(view)
+
             golgo = ' '.join(name)
-            logger.info(f'[{cbox.seat[seat]}] HCP={m.hcp:02d} card=[{golgo}]')
+            cont = series(card=m.card)
+            logger.info(f'[{cbox.seat[seat]}] HCP={m.hcp:02d} cont={cont} card=[{golgo}]')
 
 
     main()
